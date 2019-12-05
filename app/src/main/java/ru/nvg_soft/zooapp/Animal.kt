79555ec280838c94aa7fa82208d92608c0a6b2f6
CHarel <Charel@bk.ru>
2019-12-05 16:01:47 +0300

    ListView layout design

---
+++ app/src/main/java/ru/nvg_soft/zooapp/Animal.kt
@@ -0,0 +1,13 @@
+package ru.nvg_soft.zooapp
+
+class Animal {
+    var name:String? = null
+    var des:String? = null
+    var image:Int?=null
+    constructor(name:String, des:String, image:Int){
+        this.name = name
+        this.des = des
+        this.image = image
+    }
+
+}
